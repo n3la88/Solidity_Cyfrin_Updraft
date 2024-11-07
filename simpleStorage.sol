e3d514c317290fc29951b53a1205b4fd49cb94dd
n3la88 <mari.montab@gmail.com>
2024-11-07 11:26:20 -0600

    8.Arrays and structs

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -12,15 +12,39 @@ contract SimpleStorage {
     bytes favoriteBytes32 = "cat";
     */
 
-    uint256 public favoriteNumber;
+    uint256 myFavoriteNumber;
+    
+    //uint256[] listFavoriteNumbers; //[0,33,46]
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+        bool isCool;
+    }
+    //Person public pat = Person({favoriteNumber:7, name:"Pat", isCool:true});
+    //Person public mariah = Person({favoriteNumber:5, name:"mariah", isCool:true});
+    
+    //static array
+    //Person[3] public listPeople;
+
+    //dymanic array
+    Person[] public listPeople;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber, bool _isCool) public {
+        Person memory newPerson = Person(_favoriteNumber, _name, _isCool);
+        listPeople.push(newPerson);
+
+       //Another option
+       //listPeople.push( Person(_favoriteNumber, _name, _isCool) );
+    }
 
     function store(uint256 _favoriteNumber) public{
-       favoriteNumber = _favoriteNumber;
+       myFavoriteNumber = _favoriteNumber;
     }
 
     // view (does not modify state), pure () 
     function retrieve() public view returns (uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 }
 
