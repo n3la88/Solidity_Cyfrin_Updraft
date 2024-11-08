c267c8c168e689c1f909c45cf4475588800ff1c9
n3la88 <mari.montab@gmail.com>
2024-11-07 21:40:07 -0600

    Deploying the contract

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -6,7 +6,7 @@ contract SimpleStorage {
    /* 
     bool hasFavoriteNumber = true;
     uint256 favoriteNumber = 88; //(uint256 == uint)
-    string favoriteNumberInText = "eighty-eight";
+    string favoriteNumberInText = "eighty-eight"; //string = array of bytes
     int256 favoriteInt = -88;
     address myAddress = 0x0d8B53edAeae78620c676bD403c2be866F854181 ;
     bytes favoriteBytes32 = "cat";
@@ -30,12 +30,18 @@ contract SimpleStorage {
     //dymanic array
     Person[] public listPeople;
 
+    //Mapping
+    mapping(string =>Person) public nameToFavoriteNumber;
+
+    //calldata (temporary variable cannot be modified), memory (temporary variable can be modified) and storage
     function addPerson(string memory _name, uint256 _favoriteNumber, bool _isCool) public {
         Person memory newPerson = Person(_favoriteNumber, _name, _isCool);
         listPeople.push(newPerson);
 
        //Another option
        //listPeople.push( Person(_favoriteNumber, _name, _isCool) );
+
+       nameToFavoriteNumber[_name] = newPerson;
     }
 
     function store(uint256 _favoriteNumber) public{
